8dd26a4c4dc0783b5de637dba5a550bebe6a161e
Titouan FUCHS <titouan.fuchs@gmail.com>
2022-09-06 13:19:25 +0200

    feat: creating base class for shapes

---
+++ basicForms/Models/Shape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicForms.Models
+{
+    public class Shape
+    {
+        private string _Name = "Nouvelle forme";
+        
+        private double _Lenght = 0;
+        private double _Width = 0;
+
+        public Shape(string name, double lenght = 1, double width = 1)
+        {
+            _Lenght = lenght;
+            _Width = width;
+            _Name = name;
+        }
+
+        public double Surface { get => _Width * _Lenght; }
+        public double Width { get => _Width; }
+        public double Lenght { get => _Lenght; }
+        public string Name { get => _Name; }
+    }
+}
