09b46c5adf91d1aff573d271588a8995ce4959eb
Titouan FUCHS <titouan.fuchs@gmail.com>
2022-09-07 16:27:43 +0200

    feat: implementing Shape3DGroup and CalculateTotalVolume Method

--- basicForms/Program.cs
+++ basicForms/Program.cs
@@ -4,13 +4,14 @@ using basicForms.Models.Shapes;
 
 Console.WriteLine("Basic Shapes - UHA 4.0.2");
 
-int ShapeCount = 1;
+int ShapeCount = 20;
 double total = 0;
 Random rnd = new Random();
 
 Console.WriteLine("");
 
-ShapeGroup shapes = new ShapeGroup("Mon super groupe");
+ShapeGroup shapes = new ShapeGroup("Groupe de formes");
+Shape3DGroup shapes3D = new Shape3DGroup("Groupe de formes 3D");
 
 for(int i = 0; i < ShapeCount; i++)
 {
@@ -30,6 +31,26 @@ for(int i = 0; i < ShapeCount; i++)
     }
 }
 
-shapes.AddShape(new RectangleShape3D("Rectangle 3D"));
+for (int i = 0; i < ShapeCount; i++)
+{
+    int rand = rnd.Next(0, 3);
+
+    switch (rand)
+    {
+        case 0:
+            shapes3D.AddShape(new RectangleShape3D($"Rectangle_{Guid.NewGuid()}", rnd.Next(1, 1000), rnd.Next(1, 1000), rnd.Next(1, 1000)));
+            break;
+        case 1:
+            shapes3D.AddShape(new CircleShape3D($"Circle_{Guid.NewGuid()}", rnd.Next(1, 1000), rnd.Next(1, 1000)));
+            break;
+        case 2:
+            shapes3D.AddShape(new TriangleShape3D($"Triangle_{Guid.NewGuid()}", rnd.Next(1, 1000), rnd.Next(1, 1000), rnd.Next(1, 1000), rnd.Next(1, 1000)));
+            break;
+    }
+}
 
 shapes.Display();
+
+Console.WriteLine("");
+
+shapes3D.Display();
