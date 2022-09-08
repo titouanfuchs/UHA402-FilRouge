782b0a32b315e25f1a230009c172d7d109aaa7e7
Titouan FUCHS <titouan.fuchs@gmail.com>
2022-09-08 10:25:46 +0200

    feat: adding api base routes

---
+++ ShapeAPI/Controllers/ShapeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShapeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShapeController : ControllerBase
+    {
+        [HttpGet()]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpPatch()]
+        public async Task<IActionResult> Edit(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete()]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("Shape")]
+        public async Task<IActionResult> CreateShape()
+        {
+            return Ok();
+        }
+
+        [HttpPost("Group")]
+        public async Task<IActionResult> CreateGroup()
+        {
+            return Ok();
+        }
+
+        [HttpPost("AddShapeToGroup")]
+        public async Task<IActionResult> AddShapeToGroup(int shapeID, int groupID)
+        {
+            return Ok();
+        }
+    }
+}
