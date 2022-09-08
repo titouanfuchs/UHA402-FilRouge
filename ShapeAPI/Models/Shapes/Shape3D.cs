4cdc94b0f1f40894d3b8915309381852bb185a09
Titouan FUCHS <titouan.fuchs@gmail.com>
2022-09-08 21:21:31 +0200

    refactor: Shape3D

--- ShapeAPI/Models/Shapes/Shape3D.cs
+++ ShapeAPI/Models/Shapes/Shape3D.cs
@@ -2,6 +2,7 @@
 {
     public class Shape3D
     {
+        #region Fields
         private BaseShape _Shape;
         public BaseShape Shape { get => _Shape; }
         public BaseShape SetShape { set => _Shape = value; }
@@ -9,12 +10,12 @@
         private double _Height = 1;
         public double SetHeight { set => _Height = value; }
         public double Height { get => _Height; }
+        public double Volume { get => _Shape.CalculateSurface() * _Height; }
+        #endregion
 
         public Shape3D(BaseShape shape)
         {
             _Shape = shape;
         }
-
-        public double Volume { get => _Shape.CalculateSurface() * _Height; }
     }
 }
