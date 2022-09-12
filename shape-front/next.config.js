faebeb8f677535441805d85dc4da4ac2f7d1de51
Titouan FUCHS <titouan.fuchs@gmail.com>
2022-09-12 16:57:29 +0200

    feat:  adding nextjs router

--- shape-front/next.config.js
+++ shape-front/next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+    swcMinify: true,
+    webpack: config => {
+        config.watchOptions = {
+            poll: 800,
+            aggregateTimeout: 300,
+        };
+        return config;
+    },
+    rewrites: proxy => {
+        proxy = [
+            {
+                source: "/shapeAPI/:path*",
+                destination: "http://shape-api:80/:path*"
+            }
+        ];
+
+        return proxy;
+    }
+
 }
 
 module.exports = nextConfig
